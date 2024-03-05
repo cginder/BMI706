6a8801ed48a1ca05ff2ca4f81a65567fc10ebfe6
cginder <35152229+cginder@users.noreply.github.com>
2024-03-05 14:28:13 -0500

    test multiple selectors

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,27 +7,67 @@ import streamlit as st
 #Open Tasks
 #[] combine search term trends into one annual metric (average? Last?)
 #[] get absolute/relative state level trends by multiplying state trend_value * trend value for that year
+#[] ?create radio buttons to turn on/off filtering by each condition
 
-# Read Data
+##Read Data
 mortality_df = pd.read_csv("https://raw.githubusercontent.com/cginder/BMI706/main/Data/Merged%20Data/mortality_data.csv")
 gtrend_US_df = pd.read_csv("https://raw.githubusercontent.com/cginder/BMI706/main/Data/Merged%20Data/search_US_trends.csv")
 gtrend_state_df = pd.read_csv("https://raw.githubusercontent.com/cginder/BMI706/main/Data/Merged%20Data/search_state_based.csv")
 
+##Data Processing
+#Combine US Trend Data Into Annual Data
+gtrend_US_df['Year'] = gtrend_US_df['Month'].dt.year #convert to date/time format
+annual_avg_df = gtrend_US_df.groupby(['Year', 'Search_Term'])['Trend_Value'].mean().reset_index() #calculate average per year
+annual_avg_df.rename(columns={'Trend_Value': 'Annual_Avg_Trend_Value'}, inplace=True) #rename for clarity
 
-#Select Trends
+#Merge State Level Data to Get Relative Ranking (Annual Search Term Value * Relative Search Term Value for that state, that year)
+merged_df = pd.merge(gtrend_state_df, annual_avg_df, on=['Year', 'Search_Term'], how='left')
+merged_df['Relative_Weighting'] = merged_df['Annual_Avg_Trend_Value'] * merged_df['Search_Term_Value']
+
+#Convert Age Group Codes to Factors
+age_group_mapping = {
+    '15-24': 1,
+    '25-34': 2,
+    '35-44': 3,
+    '45-54': 4,
+    '55-64': 5,
+    '65-74': 6,
+    '75-84': 7,
+    '85+': 8  
+}
+age_group_codes = list(age_group_mapping.keys())
+mortality_df['Age_Group_Factor'] = mortality_df['Ten-Year Age Groups Code'].map(age_group_mapping)
+
+##Master Filter Chart
+subset = mortality_df
+
+year = st.slider('Year',1999,2019,2009)
+sex = st.radio("Sex",["Male","Female"])
+age_group_start_factor, age_group_end_factor = st.select_slider(
+    'Select Age Group(s):",
+    min_value=0,
+    max_value=len(age_group_codes)-1,
+    format_func=lambda x: age_group_codes[x]
+)
 trend_options = gtrend_US_df["Search_Term"].unique().tolist()
-default_trend_options = [
-    "Cigarette",
-    "Diet",
-    "Statin"
-]
-trends = st.multiselect("Search_Term",trend_options,default_trend_options)
+default_trend_values = ["Cigarette","Diet","Statin"]
+trends = st.multiselect("Search_Term",
+    options = trend_options,default = default_trend_values)
 
-subset = gtrend_US_df[gtrend_US_df["Search_Term"].isin(trends)]
+subset = subset[(subset['Age_Group_Factor'] >= age_group_start_factor) &
+    (subset['Age_Group_Factor'] <= age_group_end_factor)
+]
+subset = subset[subset['Sex'] == sex]
+subset = subset[subset['Year'] == year]
+
+
+
+
+trend_subset = gtrend_US_df[gtrend_US_df["Search_Term"].isin(trends)]
 
 
 #Test Plot
-chart = alt.Chart(subset).mark_line().encode(
+chart = alt.Chart(trend_subset).mark_line().encode(
     x=alt.X("Month"),
     y=alt.Y("Trend_Value"),
     color="Search_Term"
