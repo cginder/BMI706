4804b070ef38a67b08c7446c8bdc20eb92641dce
cginder <35152229+cginder@users.noreply.github.com>
2024-03-06 08:35:09 -0500

    heat

--- streamlit_app.py
+++ streamlit_app.py
@@ -233,6 +233,6 @@ def calculate_correlation(group):
     return group['Annual_Avg_Trend_Value'].corr(group['Mortality_Rate'])
 
 # Group by 'cause_of_death' and apply the correlation calculation function
-correlation_by_cause = heatmap_df.groupby('cause_of_death').apply(calculate_correlation)
+correlation_by_cause = heatmap_df.groupby(['Search_Term','cause_of_death']).apply(calculate_correlation)
 
 st.write(correlation_by_cause.head())
