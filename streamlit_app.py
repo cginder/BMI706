8e00786e5fd34fded786d5b10031debbd5d99427
cginder <35152229+cginder@users.noreply.github.com>
2024-03-06 00:58:30 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -166,7 +166,7 @@ chart4 = alt.Chart(state_average_mortality_rate).mark_line(point=True).encode(
            "subtitle":f"Selected outcomes: {outcomes_title}"},
     width=550
 )
-str.altair_chart(chart4,use_container_width=True)
+st.altair_chart(chart4,use_container_width=True)
 
 #Connected Scatter Plots
 connected_scatter_df  = pd.merge(trend_subset_state_df,state_average_mortality_rate,on=['State','Year'],how='inner')
@@ -181,7 +181,7 @@ chart5 = alt.Chart(connected_scatter_df).mark_line().encode(
     order="Year:O",
     color="State:N",
     opacity=alt.condition(
-        alt.datum.Year == 2013,
+        alt.datum.Year == select_year,
         alt.value(1),
         alt.value(0.05)
     ),
