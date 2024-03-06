917f0e9dfc8c94be07f05588113ceafb0d3cfb3e
cginder <35152229+cginder@users.noreply.github.com>
2024-03-06 16:48:22 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -347,7 +347,7 @@ chart8 = alt.Chart(lag_points_df).transform_filter(
     search_heat_selection
 )
 
-regression_line = chart8.transform_regression('Annual_Avg_Trend_Value','Mortality_Rate',method="linear").mark_line().encode(
+regression_line = chart8.transform_regression('Annual_Avg_Trend_Value','Mortality_Rate').mark_line().encode(
     color="grey",
     strokeDash=[5,5]
 
