f70eb2bd06c99892ae6d44391d824c5d1a156258
cginder <35152229+cginder@users.noreply.github.com>
2024-03-06 02:00:19 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -182,9 +182,9 @@ points = alt.Chart(connected_scatter_df).mark_point().encode(
     x=alt.X("Relative_Weighting:Q",title="Relative Search Trend"),
     y=alt.Y("Mortality_Rate:Q",title="Mortality Rate per 100,000"),
     order="Year:O",
-    color=alt.condition(state_selection,
+    color=alt.Color(alt.condition(state_selection,
                         "State:N",
-                        alt.value("White")),
+                        alt.value("Grey")),title="Select State:",
     opacity=alt.condition(
         'datum.Year < SelectorName',
         alt.value(1),
