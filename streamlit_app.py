6f7bbfea557b33671cfd8f09eb7393149c22faa0
cginder <35152229+cginder@users.noreply.github.com>
2024-03-06 17:09:17 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -349,7 +349,7 @@ chart8 = alt.Chart(lag_points_df).transform_filter(
 
 regression_line = chart8.transform_regression(
     'Annual_Avg_Trend_Value', 'Mortality_Rate', method="linear"
-).mark_line().encode(
+).mark_line(
     color='lightgrey', strokeDash=[5, 5]
 )
 combined_chart = alt.vconcat(chart7,chart8+ regression_line)
